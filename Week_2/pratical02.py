550169eeb5544793fd0a9c81968ef19d3c653435
Sam Mercer <sam@mercerfamily.org.uk>
2017-10-03 17:01:00 +0100

    I did shit it works

--- Week_2/pratical02.py
+++ Week_2/pratical02.py
@@ -3,32 +3,32 @@ pi = math.pi
 
 def circumferenceOfCircle():
     var = int(input("enter:"))
-    print(2*var*pi)
+    return 2*var*pi
 
 def areaofCircle():
-    var = int(input("enter:"))
-    print(pi**(var**2))
+    var = int(input("Enter Radius:"))
+    return pi**(var**2)
 
 def costOfPizza():
     var = int(input("Enter Diameter:"))
-    print(((var/2)**2)*pi * 1.5)
+    return ((var/2)**2)*pi * 1.5
 
 def slopeOfLine(x1,x2,y1,y2):
-     return (y2-y1)/(x2-x1)
+    return (y2-y1)/(x2-x1)
 
 def distanceBetweenPoints(x1,x2,y1,y2):
-    print(((x2-x1)**2 + (y2-y1)**2) // 2)
+    return ((x2-x1)**2 + (y2-y1)**2) // 2
 
 def travelStatistics(speed, time):
     distance = speed * time
-    print("Distance " + str(distance) + " Distance " + str(distance) +" Fuel Efff " + str(distance/5))
+    return "Distance " + str(distance) +" Fuel Efff " + str(distance/5) + "Liters were consumed"
 
 def sumOfNumbers():
     count = int(input("Enter number of values:"))
     total = 0
     for x in range(count):
         total = total + (int(input("Enter Number " + str(x +1) + ":")))
-    print(total)
+    return total
 
 def averageOfNumbers():
     count = int(input("Enter number of values:"))
@@ -40,5 +40,5 @@ def averageOfNumbers():
 def selectCoins(total_coins):
     for current_coin in [200,100,50,20,10,5,2,1]:
         total_amount = int(total_coins/current_coin)
-        print("There are " + str(total_amount) + " of " + str(current_coin) + " Pence coins")
+        print("There are " + str(total_amount) + " of " + str(current_coin) + " coins")
         total_coins -= total_amount*current_coin
