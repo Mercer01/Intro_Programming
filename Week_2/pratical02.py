304055c9a49cdbac6410f74fe722f54de2914867
Sam Mercer <sam@mercerfamily.org.uk>
2017-10-10 09:23:36 +0100

    Fix "Bad coding" and convert from return to print

--- Week_2/pratical02.py
+++ Week_2/pratical02.py
@@ -2,33 +2,35 @@ import math
 pi = math.pi
 
 def circumferenceOfCircle():
-    var = int(input("enter:"))
-    return 2*var*pi
+    radius = int(input("Please enter the radius of your circle:"))
+    print(2*radius*pi)
 
 def areaofCircle():
-    var = int(input("Enter Radius:"))
-    return pi**(var**2)
+    radius = int(input("Enter Radius:"))
+    print(pi**(radius**2))
 
 def costOfPizza():
-    var = int(input("Enter Diameter:"))
-    return ((var/2)**2)*pi * 1.5
+    diameter = int(input("Enter the diameter of your circle:"))
+    print(((diameter/2)**2)*pi * 1.5)
 
 def slopeOfLine(x1,x2,y1,y2):
-    return (y2-y1)/(x2-x1)
+    print((y2-y1)/(x2-x1))
 
-def distanceBetweenPoints(x1,x2,y1,y2):
-    return ((x2-x1)**2 + (y2-y1)**2) // 2
+def distanceBetweenFourPoints(x1,x2,y1,y2):
+    print(math.sqrt((x2-x1)**2 + (y2-y1)**2))
 
-def travelStatistics(speed, time):
+def travelStatistics():
+    speed = int(input("Please can you input tje average speed for the journey:"))
+    time = int(input("Please enter how long the journey took:"))
     distance = speed * time
-    return "Distance " + str(distance) +" Fuel Efff " + str(distance/5) + "Liters were consumed"
+    print("Distance " + str(distance) +" Fuel Efff " + str(distance/5) + "Liters were consumed")
 
 def sumOfNumbers():
     count = int(input("Enter number of values:"))
     total = 0
     for x in range(count):
         total = total + (int(input("Enter Number " + str(x +1) + ":")))
-    return total
+    print(total)
 
 def averageOfNumbers():
     count = int(input("Enter number of values:"))
@@ -38,9 +40,8 @@ def averageOfNumbers():
     print(total/count)
 
 def selectCoins(total_coins):
+    total_coins = int(input("Please input the total amount of code:"))
     for current_coin in [200,100,50,20,10,5,2,1]:
         total_amount = int(total_coins/current_coin)
         print("There are " + str(total_amount) + " of " + str(current_coin) + " coins")
         total_coins -= total_amount*current_coin
-    
-selectCoins(8023) 
